44f3dbb691cc1e4185f5316f57adfd7881cbca95
Diego <diegocleir@gmail.com>
2022-09-30 13:27:28 -0300

    primeiro commit

---
+++ lib/Jogar.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:cara_ou_coroa/Resultado.dart';
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  const Jogar({Key? key}) : super(key: key);
+
+  @override
+  State<Jogar> createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+
+  void _exibirResultado(){
+    var itens = ["cara", "coroa"];
+    var numero = Random().nextInt(itens.length);
+    var resultado = itens[numero];
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Resultado(valor: resultado))
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("imagens/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("imagens/botao_jogar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Resultado.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Resultado extends StatefulWidget {
+  String? valor;
+  Resultado({Key? key, this.valor}) : super(key: key);
+
+  @override
+  State<Resultado> createState() => _ResultadoState();
+}
+
+class _ResultadoState extends State<Resultado> {
+  @override
+  Widget build(BuildContext context) {
+
+    //print(widget.valor);
+
+    var caminhoImagem;
+    if(widget.valor == "cara"){
+      caminhoImagem = "imagens/moeda_cara.png";
+    } else {
+      caminhoImagem = "imagens/moeda_coroa.png";
+    }
+
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(caminhoImagem),
+            GestureDetector(
+              onTap: (){
+                Navigator.pop(context);
+              },
+              child: Image.asset("imagens/botao_voltar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
